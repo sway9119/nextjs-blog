fb6505d2e0eb2144f6a5d32ab030720404b22697
sway <40759948+sway9119@users.noreply.github.com>
2022-09-03 17:32:16 +0900

    画像表示のコンポーネントを追加

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
+
+const ImageComponent = () => (
+  <div>
+    <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  </div>
+);
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
       <h2>
+        <ImageComponent></ImageComponent>
         <Link href="/">Back to home</Link>
       </h2>
     </>
